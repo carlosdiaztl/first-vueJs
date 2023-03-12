7a8292c62a556e501ef6ef533ea9d20a5dfb0e11
carlosDiaz <diaz-alzate@hotmail.com>
2023-03-12 15:22:00 -0500

    plantilla vue js laravel

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import "./bootstrap";
+import { createApp } from "vue";
+
+import App from "./components/App.vue";
+createApp(App).mount("#app");
+// primer parametro es el componente y el segundo es como lo vamos a renderizar en el html
+
+// tambien en el balde hacemos referencia de que archivos estamos llamando
+// con @vite()  en cual lleva dentro la ruta de la caprtet de este archiv
